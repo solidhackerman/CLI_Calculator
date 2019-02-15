12a49790fc27742f3f16cbdff703061b72eee791
solidhackerman <vihaansharma@vihaans-macbook-air.local>
2019-02-15 11:31:55 +0530

    Initial Commit

---
+++ Calculator_Swift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Calculator_Swift
+//
+//  Created by Vihaan Sharma on 15/02/19.
+//  Copyright © 2019 Vihaan Sharma. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
